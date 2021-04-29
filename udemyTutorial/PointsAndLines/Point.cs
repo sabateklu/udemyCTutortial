ddba434f65829bdfab0524837595c5be7891f362
Saba Teklu <saab.teklu@gmail.com>
2021-04-29 10:17:23 -0600

    * .gitignore: udemy c# tutortial
    * udemyTutorial.sln: Solution
    
    * Point.cs: Folder created with class
    
    * Program.cs: Program to run all commands
    
    * udemyTutorial.csproj: workspace

---
+++ udemyTutorial/PointsAndLines/Point.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace PointsAndLines
+{
+    class User
+    {
+        private string username;
+        private int password;
+
+        public int Password {
+            set
+            {
+                if (value > 4 && value < 10)
+                {
+                    password = value;
+                }
+                else
+                {
+                    Console.WriteLine("oops, this is not a valid password! Please use a password  between 4 and 10");
+                }
+            }
+        }
+
+        public string Username
+        {
+            get
+            {
+                return "The username is " + username;
+            }
+        }
+
+
+        public User()
+        {
+
+        }
+
+        public User(string username)
+        {
+            this.username = username;
+     
+        }
+    }
+}
---
+++ udemyTutorial/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using PointsAndLines;
+
+namespace udemyTutorial
+{
+
+
+    class EntryPoint
+    {
+        static void Main()
+        {
+            User user = new User("Saba");
+            user.Password = 5;
+
+            Console.WriteLine(user.Username);
+        }
+    }
+}
